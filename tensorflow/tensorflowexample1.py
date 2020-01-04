f7a10636025827f75af9013a23811d7a07f2b2f6
Gautam Verma <gautamve@amazon.com>
2020-01-04 14:47:46 +0530

    Added tensorflow example for training

--- tensorflow/tensorflowexample1.py
+++ tensorflow/tensorflowexample1.py
@@ -1,15 +1,17 @@
 import numpy as np
 import tensorflow as tf
 
+from tensorflow import keras
+
 # Successive layers are defined in Sequence
-model = keras.Sequential(keras.layers.Dense(units=1, input=[1]))
+model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-X = np.array([-1, 0, 1, 2, 3, 4], dtype=int32)
-Y = np.array([-3,-1, 1, 3, 5, 7], dtype=int32)
+X = np.array([-1, 0, 1, 2, 3, 4], dtype=np.int32)
+Y = np.array([-3,-1, 1, 3, 5, 7], dtype=np.int32)
 
 # Goes thorough the training loop 500 times
 model.fit(X, Y, epochs=500)
 
-print("for 10 output it " , model.predict(10))
+print("for 10 output it " , model.predict([10]))
 
