7b5a314d207e8a03ef272859605523fe162b25f0
Gautam Verma <gautamve@amazon.com>
2020-01-06 09:44:12 +0530

    Added a utility to clean metric file

--- utils/cleanAndSave.py
+++ utils/cleanAndSave.py
@@ -5,26 +5,12 @@ import datetime
 
 import pandas as pd
 import numpy as np
-import xgboost as xgb
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-from sklearn.svm import SVC
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 # Log time-level and message for getting a running estimate
 logging.basicConfig(stream=sys.stdout, format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-
-def mergeDataframe(df1, df2, column, joinType='inner'):
-	if column is not None:
-		raise RuntimeError("Column can't be null. Please give the column value")
-	return pd.merge(df1, df2, on=column, how=joinType);
-
 
 def imputeMissingCols(df, numericCols, categoricalCols):
 	logging.info("Starting the imputer for a dataframe")
@@ -58,66 +44,23 @@ def cleanDataframe(df):
 	return imputeMissingCols(df, numericCols, categoricalCols);
 
 def saveFile(df, filenm, base_folder='/data/s3_file/'):
-	filename = base_folder + "_{}_" + filenm
-	column_filename = base_folder + "_{}_ColumnLabels" 
-	filename  = filename.format(int(datetime.datetime.now().timestamp()))    
+	filename = base_folder + "_clean_" + filenm
+	logging.info("Saving file " + filename)
+	df.to_csv(filename, columns=df.columns, index=False, header=True, mode='w')
 
-	columns_to_write = df.columns;
-
-	pickle.dump(str(columns_to_write), open(column_filename, 'wb'))
-	df_merged_set.to_csv(filename, columns=columns_to_write, index=False, header=False, mode='w')
-
-"""
-def loadDatasets(base_folder):
-	logging.info("Starting the loading of the datasets")
-	PlACEMENTS__FILENAME = base_folder + '3_10NovemberDS_Placementfeature000'
-	CONTENT_FILENAME = base_folder + '3_10NovemberDS_Contentfeature000'
-	RESOURCEBUNDLE_FILENAME = base_folder + '3_10NovemberDS_Bundlefeature000'
-	PLACEMENT_PROPERTIES_FILENAME = base_folder + '3_10November0S_7feature000'
-
-	# Make sure you download these file in the backed EC2 instance
-	metrics_df1 = pd.read_csv(base_folder + PlACEMENTS__FILENAME, skiprows=0, header=None)
-	metrics_df1.columns = ['frozen_placement_id', 'frozen_content_id', 'guarantee_percentage', 'created_by']
-	# for null guarantee fill the explicitely 0
-	if 'guarantee_percentage' in metrics_df1.columns:
-		metrics_df1 = metrics_df1.replace(np.nan, 0)
-	metrics_df1  = cleanDataframe(metrics_df1)
-	saveFile(base_folder, PlACEMENTS__FILENAME)
-
-	metrics_df2 = pd.read_csv(base_folder + CONTENT_FILENAME, skiprows=0, header=None)
-	metrics_df2.columns = ['frozen_content_id', 'component_name', 'component_namespace', 'created_by']
-	metrics_df2  = cleanDataframe(metrics_df2)
-	saveFile(base_folder, CONTENT_FILENAME)
-
-	metrics_df3 = pd.read_csv(base_folder + RESOURCEBUNDLE_FILENAME, skiprows=0, header=None)
-	metrics_df3.columns = ['frozen_content_id', 'language_code']
-	metrics_df3  = cleanDataframe(metrics_df3)
-	saveFile(base_folder, RESOURCEBUNDLE_FILENAME)
-
-	metrics_df4 = pd.read_csv(base_folder + PLACEMENT_PROPERTIES_FILENAME, skiprows=0, header=None)
-	metrics_df4.columns = ['frozen_placement_id', 'container_type', 'container_id', 'slot_names', 'merchant_id', 'site',
-	 'weblab', 'bullseye', 'is_recognized', 'parent_browse_nodes', 'store_names','start_date', 'end_date']
-
-	# Removing the some misssing datasets to reduce running time
-	metrics_df4  = metrics_df4[['frozen_placement_id', 'container_type', 'container_id', 
-				'slot_names', 'merchant_id', 'site','start_date', 'end_date']]
-
-	metrics_df4  = cleanDataframe(metrics_df4)
-	saveFile(base_folder, PLACEMENT_PROPERTIES_FILENAME)
-	logging.info("Dataset cleaned and loaded")
-"""
-
-def loadDatasets(base_folder, filename):
+def loadDataset(base_folder, filename, columns):
 	df = pd.read_csv(base_folder + filename, skiprows=0, header=None)
-	df.columns = ['frozen_placement_id', 'frozen_content_id', 'guarantee_percentage', 'created_by']
-	metrics_df1  = cleanDataframe(metrics_df1)
-	saveFile(base_folder, PlACEMENTS__FILENAME)
+	df.columns = columns
+	df  = cleanDataframe(df)
+	saveFile(df, filename, base_folder)
 
 def __main__():
 	# count the arguments
-	if len(sys.argv) < 3:
-		raise RuntimeError("Please provide the base_folder and filename")
-	trainModel(sys.argv[1])
+	if len(sys.argv) < 4:
+		raise RuntimeError("Please provide the base_folder, filename and columns(comma separated without quotes)")
+
+	columns = sys.argv[3].strip('][').split(',')
+	loadDataset(sys.argv[1], sys.argv[2], columns)
 
 #This is required to call the main function
 if __name__ == "__main__":
