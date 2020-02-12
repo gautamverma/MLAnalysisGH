795f5bc9cc97e15c61acbd9c81c8d1bf044d18e5
Gautam Verma <gautamve@amazon.com>
2020-02-12 13:12:20 +0530

    Minor fixes

--- XGBModelV2/XGB_11Feb2020.py
+++ XGBModelV2/XGB_11Feb2020.py
@@ -80,7 +80,7 @@ def label_column(df, column):
 def generateCleanFile(files, training_file_name):
 
 	if path.exists(training_file_name):
-		return pd.read_csv(training_file_name, skiprows=0, header=0)
+		logging.info("Training file is already present")
 
 	df_merged_set = loadAndMerge(files)
 	logging.info("Loading and dataset merged. Display head-- ")
@@ -102,7 +102,7 @@ def generateCleanFile(files, training_file_name):
 
 	logging.info("Dataframe Shape "+str(df_merged_set.shape))
 	with open(training_file_name, 'w') as csv_file:
-		df_merged_set.to_csv(path_or_buf=csv_file, index=False, encoding='utf-8', chunksize=CHUNKSIZE)
+		df_merged_set.to_pickle(csv_file)
 	logging.info('File Created')
 
 def trainModel(learning_rate, max_depth, training_file_name):
