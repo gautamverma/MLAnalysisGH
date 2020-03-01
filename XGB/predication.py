48fa5c8fa2d645514c23b2adeff2cd7c872d7439
Gautam Verma <gautamve@amazon.com>
2020-03-01 15:40:14 +0530

    minor fix

--- XGB/predication.py
+++ XGB/predication.py
@@ -65,6 +65,8 @@ def predictXGBModel(data_input, xg_reg):
         logging.info('Report : ')
         logging.info(str(classification_report(OUTPUT, np.around(predictions))))
         chunk_accuracy[chunkcount] = accuracy
-
+        chunkcount = chunkcount + 1
+        if chunkcount == 2:
+            break
     accuracy_fn = "model_accuracy_score.sav"
-    return accuracy_fn, utils.saveDataOnDisk(accuracy, data_input[const.IFOLDER_KEY] + accuracy_fn)
+    return accuracy_fn, utils.saveDataOnDisk(chunk_accuracy, data_input[const.IFOLDER_KEY] + accuracy_fn)
