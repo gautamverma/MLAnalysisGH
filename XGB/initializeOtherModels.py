243270cdcf9ff4943e327d388e46a12337e60392
Gautam Verma <gautamve@amazon.com>
2020-03-30 21:48:40 +0530

    Added support in other models to train on other files

--- XGB/initializeOtherModels.py
+++ XGB/initializeOtherModels.py
@@ -1,7 +1,8 @@
 import sys
 import datetime
 import logging
-import sys
+
+from sys import path
 
 import constants as const
 import data_filters as filters
@@ -15,13 +16,18 @@ def start_steps(bucket, jsonprefix, base_folder):
     data_input = prepareInputData(bucket, jsonprefix, base_folder)
     generateCleanFile(data_input)
 
+    training_file = data_input[const.IMULTIPLE_TRAINING_FILE]
+    if not path.exists(training_file):
+        training_file = data_input[const.ITRAINING_FP]
+    logging.info ("Train and testing on the " + training_file)
+
     utils.logBreak()
-    data_input = filters.filterProdEnviroment(data_input, data_input[const.IMULTIPLE_TRAINING_FILE])
+    data_input = filters.filterProdEnviroment(data_input, training_file)
     timestamp_value = int (datetime.datetime.now ().timestamp ())
     buildPredicationModel(data_input, data_input[const.PROD_ENVIROMENT_FILTERED_FILE], data_input[const.IPREFIX_KEY] + str (timestamp_value) + "_ProdFiltered/")
 
     utils.logBreak()
-    data_input = filters.filterNonMarketingData(data_input, data_input[const.IMULTIPLE_TRAINING_FILE])
+    data_input = filters.filterNonMarketingData(data_input, training_file)
     timestamp_value = int (datetime.datetime.now ().timestamp ())
     buildPredicationModel(data_input, data_input[const.NON_MARKETING_FILTERED_FILE], data_input[const.IPREFIX_KEY] + str (timestamp_value) + "_NonMarketingFiltered/")
 
