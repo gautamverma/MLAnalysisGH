2487ff0098e42baf574e2998878dc6801deaba90
Gautam Verma <gautamve@amazon.com>
2020-04-06 20:13:52 +0530

    Modified the tuning job

--- XGB/singleFileXGBProcessing/TrainAndTest.py
+++ XGB/singleFileXGBProcessing/TrainAndTest.py
@@ -19,8 +19,16 @@ from resultFunctions import callFunctionByName
 logging.basicConfig(stream=sys.stdout, format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def trainXGBModel(data_input, training_filepath):
-	# Init a base Model
-	xg_reg = {}
+	# Init a variable to keep the best Model
+	xgb_model = {}
+	otherValues = {
+		'accuracy_value': 0
+	}
+
+	# Initialize the range to test the different parameters
+	learning_rate_vals = np.arange (0.1, 0.5, 0.02)
+	max_depth_vals = np.arange (8, 24, 1)
+	n_estimators_vals = np.arange (50, 500, 10)
 
 	# Predication will be always on 1 result col
 	YColumns = [data_input[constants.IRESULT_COL_KEY]]
@@ -36,31 +44,47 @@ def trainXGBModel(data_input, training_filepath):
 
 	df = df[columns_to_keep]
 	Y, X = df.iloc[:, 0], df.iloc[:, 1:]
-	X_train, X_test, y_train, y_test = train_test_split (X, Y, test_size=0.2, random_state=123)
 
-	numeric_data = X_train.iloc[:,0:len(numericalCols)]
-	one_hot_encoded = one_hot_encoder.transform(X_train.iloc[:,len(numericalCols):])
-	d_train = xgb.DMatrix (np.column_stack ((numeric_data, one_hot_encoded)), label=y_train)
+	learning_parameters = data_input[constants.IPARAMS_KEY]
+	for learning_rate in learning_rate_vals:
+		for max_depth in max_depth_vals:
+			for n_estimators in n_estimators_vals:
+				learning_parameters['max_depth'] = max_depth
+				learning_parameters['n_estimators'] = n_estimators
+				learning_parameters['learning_rate'] = learning_rate
+				data_input[constants.IPARAMS_KEY] = learning_parameters
 
-	xg_reg = xgb.train(data_input[constants.IPARAMS_KEY], d_train, data_input[constants.ITRAIN_ITERATIONS])
+				X_train, X_test, y_train, y_test = train_test_split (X, Y, test_size=0.2, random_state=123)
 
-	numeric_data = X_test.iloc[:,0:len(numericalCols)]
-	one_hot_encoded = one_hot_encoder.transform(X_test.iloc[:,len(numericalCols):])
-	d_test = xgb.DMatrix(np.column_stack ((numeric_data, one_hot_encoded)))
-	preds = xg_reg.predict(d_test)
+				numeric_data = X_train.iloc[:,0:len(numericalCols)]
+				one_hot_encoded = one_hot_encoder.transform(X_train.iloc[:,len(numericalCols):])
+				d_train = xgb.DMatrix (np.column_stack ((numeric_data, one_hot_encoded)), label=y_train)
 
-	accuracy = accuracy_score(y_test, np.around(preds))
-	matrix = confusion_matrix(y_test, np.around (preds))
+				xg_reg = xgb.train(data_input[constants.IPARAMS_KEY], d_train, data_input[constants.ITRAIN_ITERATIONS])
 
-	logging.info(str(data_input[constants.IPARAMS_KEY]))
-	logging.info('Confusion Matrix : ')
-	logging.info(str(matrix))
-	logging.info('Accuracy Score : ' + str (accuracy))
-	logging.info(str(classification_report (y_test, np.around (preds))))
-	logging.info("\n\n\n")
+				numeric_data = X_test.iloc[:,0:len(numericalCols)]
+				one_hot_encoded = one_hot_encoder.transform(X_test.iloc[:,len(numericalCols):])
+				d_test = xgb.DMatrix(np.column_stack ((numeric_data, one_hot_encoded)))
+				preds = xg_reg.predict(d_test)
 
-	return xg_reg
+				accuracy = accuracy_score(y_test, np.around(preds))
+				matrix = confusion_matrix(y_test, np.around (preds))
 
+				logging.info(str(data_input[constants.IPARAMS_KEY]))
+				logging.info('Confusion Matrix : ')
+				logging.info(str(matrix))
+				logging.info('Accuracy Score : ' + str (accuracy))
+				logging.info(str(classification_report (y_test, np.around (preds))))
 
+				if(otherValues['accuracy_value']<accuracy):
+					xgb_model = xg_reg
+					otherValues['accuracy_value'] = accuracy
+					otherValues['Confusion_Matrix'] = matrix
+					otherValues['Classification_report'] = classification_report(y_test, np.around (preds))
+			logging.info("\n")
+		logging.info('\n')
+	logging.info('\n')
 
-
+	logging.info('Best Model values')
+	logging.info('')
+	return xgb_model
--- XGB/singleFileXGBProcessing/initializeSingleFile.py
+++ XGB/singleFileXGBProcessing/initializeSingleFile.py
@@ -1,9 +1,7 @@
 import datetime
-import logging
-import numpy as np
-
 # To prepare the other modules
 import sys
+
 sys.path.append('..')
 
 from utility import constants
@@ -20,33 +18,10 @@ def start_steps(data_input, base_folder):
     data_input[constants.IMODEL_FN] = str(int(datetime.datetime.now().timestamp())) + data_input[constants.IMODEL_FN]
     data_input[constants.IMODEL_FP] = base_folder + data_input[constants.IMODEL_FN]
     utils.saveDataOnDisk (xgb_model, data_input[constants.IMODEL_FP])
-    return
 
 def hyperParametersTuning(bucket, jsonprefix, base_folder):
     data_input = prepareInputData (bucket, jsonprefix, base_folder)
-
-    learning_parameters = {
-        'objective': 'binary:logistic',
-        'colsample_bytree': 0.3,
-        'learning_rate': 0.1,
-        'max_depth': 8,
-        'alpha': 5,
-        'n_estimators': 50
-    }
-
-    learning_rate_vals = np.arange(0.1, 0.5, 0.02)
-    max_depth_vals     = np.arange(8, 24, 1)
-    n_estimators_vals  = np.arange(50, 500, 10)
-
-    for learning_rate in learning_rate_vals:
-        for max_depth in max_depth_vals:
-            for n_estimators in n_estimators_vals:
-                learning_parameters['max_depth'] = max_depth
-                learning_parameters['n_estimators'] = n_estimators
-                learning_parameters['learning_rate'] = learning_rate
-                data_input[constants.IPARAMS_KEY] = learning_parameters
-                start_steps(data_input, base_folder)
-    return
+    start_steps (data_input, base_folder)
 
 def __main__():
     # count the arguments
