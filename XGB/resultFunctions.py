ac7046c2b45808e0fe3738e70defc205826f1482
Gautam Verma <gautamve@amazon.com>
2020-05-07 00:07:42 +0530

    Minor fix

--- XGB/resultFunctions.py
+++ XGB/resultFunctions.py
@@ -1,56 +1,56 @@
 def use10Classifer(row):
-	if(row['impressions']<10):
+	if row['impressions']<10:
 		return 0
 	return 1
 
 def useDigitClassifier5ValueSet(row):
-	if(row['impressions']<10):
+	if row['impressions']<10:
 		return 0
-	elif(row['impressions']<100):
+	elif row['impressions']<100:
 		return 1
-	elif(row['impressions']<1000):
+	elif row['impressions']<1000:
 		return 2
-	elif(row['impressions']<10000):
+	elif row['impressions']<10000:
 		return 3
 	return 4
 
 def useDigitClassifier6ValueSet(row):
-	if(row['impressions']<10):
+	if row['impressions']<10:
 		return 0
-	elif(row['impressions']<100):
+	elif row['impressions']<100:
 		return 1
-	elif(row['impressions']<1000):
+	elif row['impressions']<1000:
 		return 2
-	elif(row['impressions']<10000):
+	elif row['impressions']<10000:
 		return 3
-	elif(row['impressions']<100000):
+	elif row['impressions']<100000:
 		return 4
 	return 5	
 
 def useDigitClassifier7ValueSet(row):
-	if(row['impressions']<10):
+	if row['impressions']<10:
 		return 0
-	elif(row['impressions']<100):
+	elif row['impressions']<100:
 		return 1
-	elif(row['impressions']<1000):
+	elif row['impressions']<1000:
 		return 2
-	elif(row['impressions']<10000):
+	elif row['impressions']<10000:
 		return 3
-	elif(row['impressions']<100000):
+	elif row['impressions']<100000:
 		return 4
-	elif(row['impressions']<1000000):
+	elif row['impressions']<1000000:
 		return 5	
 	return 6
 
 def callFunctionByName(row, functionName):
-	if(functionName == 'use10Classifer'):
+	if functionName == 'use10Classifer':
 		return use10Classifer(row)
-	elif (functionName == 'useDigitClassifier5ValueSet'):
+	elif functionName == 'useDigitClassifier5ValueSet':
 		return useDigitClassifier5ValueSet(row)
-	elif (functionName == 'useDigitClassifier6ValueSet'):
+	elif functionName == 'useDigitClassifier6ValueSet':
 		return useDigitClassifier6ValueSet(row)
-	elif (functionName == 'useDigitClassifier7ValueSet'):
+	elif functionName == 'useDigitClassifier7ValueSet':
 		return useDigitClassifier7ValueSet(row)
-	elif(functionName == 'useImpressions')
+	elif functionName == 'useImpressions':
 		return row['impressions']
 	return use10Classifer(row)
